608882062371cec62377c755701c641318b7fe86
Crisjer Gutana <crisjergutana@gmail.com>
2026-02-11 16:04:28 +0800

    test push

--- login.php
+++ login.php
@@ -1,26 +1,26 @@
 <?php
-session_start();
-require 'db.php';
+// session_start();
+// require 'db.php';
 
-$error = "";
+// $error = "";
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+// if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+//     $username = $_POST['username'];
+//     $password = $_POST['password'];
 
-    $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
-    $stmt->execute([$username]);
-    $user = $stmt->fetch();
+//     $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
+//     $stmt->execute([$username]);
+//     $user = $stmt->fetch();
 
-    if($user && password_verify($password, $user['password'])){
-        $_SESSION['user'] = $user['fullname'];
-        header("Location: dashboard.php");
-        exit;
-    }else{
-        $error = "Invalid username or password.";
-    }
-}
+//     if($user && password_verify($password, $user['password'])){
+//         $_SESSION['user'] = $user['fullname'];
+//         header("Location: dashboard.php");
+//         exit;
+//     }else{
+//         $error = "Invalid username or password.";
+//     }
+// }
 ?>
 
 <!DOCTYPE html>
