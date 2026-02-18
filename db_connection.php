c7e0077864f4a88c7038f2509d20cfab94f1e183
Merlogic <makilanmerjie455@gmail.com>
2026-02-18 10:27:08 -0800

    updating the connection

--- db_connection.php
+++ db_connection.php
@@ -1,14 +1,15 @@
 <?php
 $host = "localhost";       
-$dbname = "clp";           
+$dbname = "clp_db";  
 $user = "root";             
 $pass = "";                 
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Connected successfully"; // Uncomment to test connection
 } catch(PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
 ?>
+
+
